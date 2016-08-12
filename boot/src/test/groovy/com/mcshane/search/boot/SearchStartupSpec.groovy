88366482ce9b98049c901efd116413a2d03e7f72
mcshanej <jmcshane@elementcorp.com>
2016-08-12 00:35:49 -0500

    Resolving unit test failures by containing search files within the project.  Need to create new method for integration tests still

--- boot/src/test/groovy/com/mcshane/search/boot/SearchStartupSpec.groovy
+++ boot/src/test/groovy/com/mcshane/search/boot/SearchStartupSpec.groovy
@@ -5,11 +5,13 @@ import org.springframework.boot.test.context.SpringBootContextLoader
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.ActiveProfiles
 
 import spock.lang.Specification
 
 @ContextConfiguration(loader = SpringBootContextLoader.class, classes = Application.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class SearchStartupSpec extends Specification {
 
 	@Autowired
