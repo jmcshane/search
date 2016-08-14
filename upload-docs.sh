31c7a6a4bc3da2930fa218a2378d45c3d5ba8a82
mcshanej <jmcshane@elementcorp.com>
2016-08-13 22:29:01 -0500

    Using travis HOME to create a new directory for the gh-pages rather than doing it within the build dir

--- upload-docs.sh
+++ upload-docs.sh
@@ -1,16 +1,21 @@
 
 DATE_FOLDER=$(date +"%m-%d-%y-%H-%M")
+
+cp -R build/reports/ $HOME/reports 
+
+cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/jmcshane/search gh-pages > /dev/null
 
+mkdir -p gh-pages/$DATE_FOLDER/benchmark
+
+cp -R reports gh-pages/$DATE_FOLDER/
+
 cd gh-pages
-mkdir -p $DATE_FOLDER/benchmark
-
-cd ..
-
-cp -R build/reports/ gh-pages/$DATE_FOLDER/
 
 git add -f .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git remote -v
+git status
 git push -fq origin gh-pages > /dev/null
