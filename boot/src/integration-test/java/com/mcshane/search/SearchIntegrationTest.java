beac76b453cfec762ba206a4d7cc3e48dc00ea20
mcshanej <jmcshane@elementcorp.com>
2016-08-13 23:02:18 -0500

    Simple change to see if the compile integration test failure was a one-off and separating the iTest from other actions

--- boot/src/integration-test/java/com/mcshane/search/SearchIntegrationTest.java
+++ boot/src/integration-test/java/com/mcshane/search/SearchIntegrationTest.java
@@ -12,7 +12,6 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang3.StringUtils;
